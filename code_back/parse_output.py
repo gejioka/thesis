662afc1b897b2c34ef919e7c4775a64a5d055169
gejioka <ge.tziokas@gmail.com>
2019-07-09 17:32:22 +0300

    Delete some folders from project.

--- code_back/parse_output.py
+++
@@ -1,7 +0,0 @@
-with open("output.txt","r") as f:
-    all_nodes = 0
-    dominators = 0
-    for line in f:
-        dominators +=1 
-        all_nodes += int(line.split(" ")[1].replace(")","").strip())
-    print all_nodes + dominators
